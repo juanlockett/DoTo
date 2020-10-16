0e7b0bff90b9d54f964e127596fee85bcd24a116
Juan Lockett <juanlockett@gmail.com>
2020-10-16 10:59:44 -0300

    se agrega rendereado de template y la estructura para contener los templates html

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, request, redirect, make_response, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    user_ip = request.remote_addr
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+@app.route('/hello')
 def hello():
-    return 'Hello World Flask'
+    user_ip = request.cookies.get('user_ip')
+
+    return render_template('hello.html', user_ip=user_ip)
