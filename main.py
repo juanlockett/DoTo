a0203519387fcac308c14f2a79d7df2cc961f378
Juanjo <juanlockett@gmail.com>
2020-10-17 17:59:53 -0300

    se implementa post y se guardan los datos del formulario en la sesion

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, make_response, render_template, session
+from flask import Flask, request, redirect, make_response, render_template, session, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
@@ -34,14 +34,24 @@ def index():
 
     return response
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
     user_ip = session.get('user_ip')
     login_form = LoginForm()
+    username = session.get('username')
+
+
     context = {
         'user_ip': user_ip,
         'todos': todos,
         'login_form': login_form,
+        'username': username,
     }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        return redirect(url_for('index'))
 
     return render_template('hello.html', **context)
