ef80e0db01a9b88c80c9f6d424692aa923691e89
Juanjo <juanlockett@gmail.com>
2020-10-17 18:14:07 -0300

    se implementan los alertas en flask mensajes que se despliegan por debajo del navbar, para esto tambine tubimos que importar el bock script de bootstrap

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, make_response, render_template, session, url_for
+from flask import Flask, request, redirect, make_response, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
@@ -51,6 +51,8 @@ def hello():
     if login_form.validate_on_submit():
         username = login_form.username.data
         session['username'] = username
+
+        flash('Nombre de usuario registrado con éxito!')
 
         return redirect(url_for('index'))
 
