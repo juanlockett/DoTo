6e42608de8349a213344cb64407ae93e99da2d51
Juanjo <juanlockett@gmail.com>
2020-10-22 21:20:47 -0300

    refactorizando la applicación y aplicando bluepronts
    Se trabaja con flask Factory para mover los archivos a las carpetas donde pertenencen y se aplican los blueprints para poder tener el aplicativo dividido en modulos que seran como micro aplicaciones"

---
+++ app/config.py
@@ -0,0 +1,2 @@
+class Config():
+    SECRET_KEY = 'SETEAR UNA CLAVE SECRETA'#cambiar esta llave por una llave generada programaticamente
--- main.py
+++ main.py
@@ -1,24 +1,12 @@
-from flask import Flask, request, redirect, make_response, render_template, session, url_for, flash
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from flask import request, redirect, make_response, render_template, session, url_for, flash
+from app.forms import LoginForm
+
 import unittest
+from app import create_app
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
-
-app.config['SECRET_KEY'] = 'SETEAR UNA CLAVE SECRETA'#cambiar esta llave por una llave generada programaticamente
-
+app = create_app()
 
 todos = ['Comprar café', 'enviar video al productor', 'terminar dashboard rrss']
-
-
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de usuario', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
-
 
 @app.cli.command()
 def test():
