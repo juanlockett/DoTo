9dcdd4b47b8a5d173ff7d0ebf79a7da8cdd4f9d9
Juanjo <juanlockett@gmail.com>
2020-10-24 12:45:37 -0300

    se agrega el servicio de conexión a firestore
    se agregan tambien metodos para traer usuarios y todos

---
+++ app/firestore_service.py
@@ -0,0 +1,21 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+#project_id = 'testflask-266417'
+credential = credentials.ApplicationDefault()
+firebase_admin.initialize_app(credential)
+
+# , {
+#     'projectID': project_id,
+# }
+
+db = firestore.client()
+
+def get_users():
+    return db.collection('users').get()
+
+def get_todos(user_id):
+    return db.collection('users')\
+            .document(user_id)\
+            .collection('todos').get()
